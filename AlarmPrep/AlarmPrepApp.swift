0060dd39431bd8d9fb290c5e2c22b91fe6853c4d
Abishan <highoncode09@gmail.com>
2023-03-17 23:35:16 +0530

    Initial Commit

---
+++ AlarmPrep/AlarmPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmPrepApp.swift
+//  AlarmPrep
+//
+//  Created by Abishan Parameswaran on 2023-03-17.
+//
+
+import SwiftUI
+
+@main
+struct AlarmPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
